7ffa7c2bb686e9a49d451f9d9fec8b49cefca658
Laurence Chen <laurence@replware.dev>
2020-05-21 17:29:29 +0800

    basic quick start test code for Backendless

--- src/cljs/gamification/core.cljs
+++ src/cljs/gamification/core.cljs
@@ -6,9 +6,7 @@
    [gamification.events :as events]
    [gamification.routes :as routes]
    [gamification.views :as views]
-   [gamification.config :as config]
-   ))
-
+   [gamification.config :as config]))
 
 (defn dev-setup []
   (when config/debug?
@@ -19,6 +17,21 @@
   (let [root-el (.getElementById js/document "app")]
     (rdom/unmount-component-at-node root-el)
     (rdom/render [views/main-panel] root-el)))
+
+(defn backendless-setup []
+  (set! (.-serverURL js/Backendless) "https://api.backendless.com")
+  (.initApp js/Backendless config/APP-ID config/API-KEY))
+
+(defn backendless-save-data []
+  (-> (.save
+       (.of (.. js/Backendless -Data) "TestTable")
+       (js-obj "foo" "bar"))
+      (.then #(js/console.log (str "Object saved. ObjectId " %)))
+      (.catch #(js/console.log (str "got error - " %)))))
+
+(defn run-backendless-test []
+  (backendless-setup)
+  (backendless-save-data))
 
 (defn init []
   (routes/app-routes)
